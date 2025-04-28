7f517a653f3906a1220bfcce478637b1c8745e5d
ricniclas <ricardoniclaslopes@gmail.com>
2025-04-28 17:32:57 -0300

    Added language option

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,149 @@
 'use strict';
 
+const language_en = 
+{
+  "jobTitle": "Game Developer",
+  "welcome": "Welcome to our websiteeee.",
+  "ptLanguage": "Portuguese",
+  "enLanguage": "English",
+  "myName": "Ricardo Niclas",
+  "showContacts": "Show Contacts",
+  "phoneTitle": "PHONE",
+  "birthdayTitle": "BIRTHDAY",
+  "birthdayValue": "March 11, 1998",
+  "locationTitle": "LOCATION",
+  "aboutMe":"About Me",
+  "about": "About",
+  "resume": "About",
+  "portfolio": "About",
+  "aboutGratuated": "Having graduated in Digital Game Development from college, I continued my career as an Android developer in the field of Information Security. Currently, I work as a Unity developer, creating medical simulations.",
+  "asAGameDeveloper":"As a game developer, I follow the best software development practices to maintain projects organized and expansive, as well as the implementation of external tools to supply the application functionalities such as servers (NodeJS), database (MySQL), and audio interface (FMOD).",
+  "IAlsoHave":"I also have experience as a technical artist, creating shaders using Shader Graph and HLSL, special effects using VFX Graph, Post Processing, Renderer Features, as well as creating supplementary meshes for VFX using Maya.",
+  "mySkills": "My Skills",
+  "gameDevelopment": "Game Development",
+  "6yearsOf":"6 years of experience developing games with Unity, including mobile, web, desktop, AR and medical simulations. Developing mechanics, APIs, input systems, data management with Scriptable Objects and SQLite, FMOD and online functionalities with Photon Pun",
+  "techArt":"Tech Art",
+  "shaderDevelopment":"Shader development for Unity and Unreal using both HLSL and Shader Graph, VFX and particle development, lighting and post processing. while also having skills with 3D and 2D softwares such as Maya, Photoshop, Premiere and After Effects",
+  "androidDevelopment":"Android Development",
+  "3yearsOf":"3 years of experience developing Android libraries focusing on infomation security, as well as app obfuscation, distribution automation and feature flags",
+  "backendDevelopment":"Backend Development",
+  "backendDevelopmentWith":"Backend development with NodeJS and Golang utilizing end to end encryption, as well as databases development with MySQL",
+  "testimonials":"Testimonials",
+  "brunoTest1":"I had the pleasure of working with Ricardo on multiple projects, especially in the information security area for Android. From the beginning, he has shown to be a talented and dedicated professional, whose technical knowledge and complex problem-solving skills were noticeable.",
+  "brunoTest2":"His innovative contributions implementing solutions about security were fundamental to the success of our projects, and besides his technical skills, Ricardo has an immense disposition to help his teammates and a strong sense of companionship.",
+  "joaoTest1":"It is a pleasure to recommend Ricardo. His contribution to the team was fundamental, from the conception to the delivery of robust security solutions for the largest banks and fintechs in Brazil.",
+  "joaoTest2":"His curiosity and openness to new opportunities set him apart, leading him to masterfully take on the role of tech lead, delivering one of Unico’s most important projects. Over the years, our collaboration has been incredible. I sincerely appreciate him being an essential part of the team. Keep striving for success, Ric!",
+  "resumeTitle":"Resume",
+  "reExperience":"Experience",
+  "reEdu":"Education",
+  "reMySkills":"My Skills",
+  "reSimtheraTitle":"Simthera, Jardim Europa-SP — Unity Developer",
+  "reSimtheraTime":"05/2024 - Currently",
+  "reSimtheraDesc":"I work as a Unity developer, responsible for the development and maintenance of medical training softwares, as well as the external tools necessary for the functionality of those applications.<br><br> ● Development of Unity applications for medical simulations<br> ● Integration of positional tracking devices with Unity<br> ● Shader development emulating medical tools<br> ● Creation and maintenance of databases and servers that enable functionalities of the simulations<br> ● Creation of assets for publicity using Maya, Photoshop, After Effects and Adobe Premiere.",
+  "reUnicoTitle":"Unico, Tatuapé-SP — Associate Developer - IC3",
+  "reUnicoTime":"09/2022 - 01/2024",
+  "reUnicoDesc":"After the acquisition of Makrosystems by Unico, I continued to focus on Android development, but participated as a Tech Lead in an important internal project. which introduced me to extra responsibilities other than what I already had at Makrosystems.<br><br> ● Developed the Github pipeline that published the product on Google Cloud Services<br> ● Assigned tasks and managed them through Jira<br> ● Represented the team to the Product Owner, reporting the tasks status and defining new business rules<br> ● Implemented a Feature Flag system on the project<br> ● Produced design docs detailing objectives, functionalities and projects prerequisites.",
+  "reMakroTitle":"Makro, Tatuapé-SP — Junior Programmer",
+  "reMakroTime":"01/2021 - 09/2022",
+  "reMakroDesc":"I worked as an Android developer, focusing on the Java language for the development of libraries aimed at information security, I developed tools such as:<br><br> ● Cryptography and Pinning tools<br> ● Devices analysis<br> ● Static and dynamic analysis of application vulnerabilities<br> ● Unit tests and demo applications<br> ● Obfuscation with Dexguard, and runtime protection with RASP <br> ● Implementation manuals for our clients" ,
+  "reIcarusTitle":"Icarus Innovation Laboratory, Carapicuíba-SP — Monitor",
+  "reIcarusTime":"06/2019 - 12/2020",
+  "reIcarusDesc":"I worked as a monitor, helping students develop their semester projects, where they developed games for numerous platforms.<br>I also worked as a supervisor and developer of projects focused on emerging technologies such as Augmented Reality, Virtual Reality using Unity. Our projects managed to receive awards and also participate in big events such as Campus Party" ,
+  "reFatecTitle":"Fatec - Digital Games Development — Technologist",
+  "reFatecTime":"07/2018 — 12/2021",
+  "reFatecDesc":"College was my starting point as a developer, where I developed programming skills in several languages such as Python, Java, and mainly C# with a focus on Unity development. It was also where I started my first extracurricular projects and game jams." ,
+  "reSagaTitle":"SAGA - School of art and games — Technical Course",
+  "reSagaTime":"06/2019 - 12/2020",
+  "reSagaDesc":"In the course, I learned about image and video editing processes, using platforms such as Photoshop, Premiere, After Effects, as well as 3D software such as Maya and Substance Painter." ,  
+  "reSkillUnity":"C# for Unity(6 years)",
+  "reSkillJava":"Java for Android (3 Years)",
+  "reSkillTech":"Tech Art (Unity and Unreal)",
+  "reSkillImage":"Image Editing (Photoshop, Illustrator, Premiere, After Effects)",
+  "reSkillUnityBackend":"Backend (NodeJS, GoLang)",
+  "reSkillUnityGit":"Github (Gitflow, Pipelines)",
+  "poSimMicsPos":"Lead Programmer - Backend",
+  "poSimRaqPos":"Lead Programmer - Backend",
+  "poStandPos":"Lead Programmer - Tech Artist",
+  "poLooopsPos":"Lead Programmer - Tech Artist - Sound Designer",
+  "poHeirsPos":"Lead Programmer",
+  "poSdanitPos":"Tech Artist",
+  "poDoorsPos":"UI - Composer",
+  "poDataPos":"Tech Artist - Animator - Composer",
+  "poEscaladaPos":"UI - Programmer"
+}
 
-
+const language_pt = 
+{
+  "jobTitle": "Desenvolvedor de Jogos",
+  "welcome": "Bem vindo ao meu site",
+  "ptLanguage": "Português",
+  "enLanguage": "Inglês",
+  "myName": "Ricardo Niclas",  
+  "showContacts": "Mostrar Contatos",
+  "phoneTitle": "TELEFONE",
+  "birthdayTitle": "ANIVERSÁRIO",
+  "birthdayValue": "11 de Março, 1998",
+  "locationTitle": "LOCALIZAÇÃO",
+  "aboutMe": "Sobre Mim",
+  "about": "Sobre",
+  "resume": "Currículo",
+  "portfolio": "Portfólio",
+  "aboutGratuated": "Formado em Desenvolvimento de Jogos Digitais na Fatec, prossegui minha carreira como desenvolvedor Android na área da segurança da informação. Apesar da mudança de área, sempre mantive meus conhecimentos no desenvolvimento de jogos atualizados participando de múltiplos projetos pessoais e vencendo Game Jams.",
+  "asAGameDeveloper":"Como desenvolvedor de jogos, possuo conhecimento em distintas áreas como o desenvolvimento de mecânicas de gameplay e sistemas para Unity, assim como a criação de shaders, partículas, VFX e trilhas sonoras dinâmicas utilizando FMOD.",
+  "iAlsoHave":"Hoje busco mesclar meus conhecimentos nas duas áreas desenvolvendo jogos com criterioso cuidado em relação aos padrões de design, segurança de código e testes, assim como também aprimorar os pipelines de distribuição.",
+  "mySkills" : "Minhas Habilidades",
+  "gameDevelopment": "Desenvolvimento de Jogos",
+  "6yearsOf":"6 anos de experiência desenvolvendo jogos com Unity, incluindo mobile, web, desktop, AR e simuladores médicos. Desenvolvendo mecânicas, APIs, sistemas de Input, gerenciamento de dados com Scriptable Objects e SQLite, FMOD e funcionalidades online com Photon Pun",
+  "techArt":"Tech Art",
+  "shaderDevelopment":"Desenvolvimento de Shader com Unity e Unreal usando HLSL e Shader Graph. criação de partículas e VFX, iluminação e o pós processamento. e também possuo habilidades com softwares 3D e 2D como Maya, Photoshop, Premiere e After Effects",
+  "androidDevelopment":"Desenvolvimento Android",
+  "3yearsOf":"3 anos de experience desenvolvendo bibliotecas Android com foco na segurança da informação, assim como ofuscação de aplicativos, automação de distribuição e feature flags",
+  "backendDevelopment":"Desenvolvimento Backend",
+  "backendDevelopmentWith":"Desenvolvimento backend com NodeJS e Golang utilizando criptografia de ponta a ponta, assim como desenvolvimento de banco de dados com MySQL",
+  "testimonials":"Recomendações",
+  "brunoTest1":"Tive o prazer de trabalhar com Ricardo em vários projetos, especialmente na área de segurança para Android. Desde o início, ele demonstrou ser um profissional talentoso e dedicado, cujo conhecimento técnico e habilidade em resolver problemas complexos são verdadeiramente notáveis.",
+  "brunoTest2":"Sua contribuição na implementação de soluções de segurança inovadoras foi fundamental para o sucesso dos nossos projetos. Além de suas habilidades técnicas, Ricardo tem uma incrível disposição para ajudar e seu forte senso de companheirismo.",
+  "brunoTest3":"Sempre esteve disponível para apoiar a equipe, compartilhar seu conhecimento e oferecer orientações valiosas. Sua capacidade de trabalhar em equipe e motivar os colegas contribuiu significativamente para um ambiente de trabalho colaborativo e produtivo.",
+  "joaoTest1":"Recomendar o Ricardo é um prazer. Sua contribuição para o time foi fundamental, desde a concepção até a entrega de soluções de segurança robustas para os maiores bancos e fintechs do Brasil.",
+  "joaoTest2":"Sua curiosidade e abertura a novas oportunidades o destacaram, levando-o a assumir com maestria o papel de tech lead, entregando um dos projetos mais importantes da Unico. Ao longo dos anos, nossa colaboração foi incrível. Agradeço sinceramente por ele ter sido parte essencial da equipe. Sucesso contínuo, Ric!",
+  "resumeTitle":"Currículo",
+  "reExperience":"Experiência",
+  "reEdu":"Formação",
+  "reMySkills":"Minhas habilidades",
+  "reSimtheraTitle":"Simthera, Jardim Europa-SP — Desenvolvedor Unity",
+  "reSimtheraTime":"05/2024 - Atualmente",
+  "reSimtheraDesc":"Trabalho como desenvolvedor Unity, sendo responsável pelo desenvolvimento e manutenção dos programas de treinamento médico da empresa. Assim como todas as ferramentas externas necessárias para a funcionalidade dessas aplicações.<br><br> ● Desenvolvimento de aplicações Unity para treinamento médico<br> ● Implementação de sistemas de sensores de posicionamento de dispositivos<br> ● Desenvolvimento de Shaders simulando ferramentas médicas<br> ● Criação de servidores e banco de dados que ampliam a funcionalidade das aplicações<br> ● Criação de assets para material publicitário com Maya, Photoshop e Premiere",
+  "reUnicoTitle":"Unico, Tatuapé-SP — Associate Developer - IC3",
+  "reUnicoTime":"09/2022 - 01/2024",
+  "reUnicoDesc":"Após a aquisição da Makrosystems pela Unico, segui com o foco no desenvolvimento Android, porém participei como Tech Lead de um projeto importante interno, o que me introduziu responsabilidades extras além das que eu já cumpria na Makrosystems.<br><br> ● Desenvolvi o Pipeline no Github para a distribuição no Google Cloud Services<br> ● Designava tarefas ao time e as gerenciava  a partir do Jira<br> ● Representava o time para o Product Owner, reportando a situação das tarefas e também definindo as novas regras de negócio do projeto.<br> ● Implementei o sistema de Feature Flags no projeto<br> ● Desenvolvi design docs detalhando os objetivos, funcionalidades e requisitos do projeto.",
+  "reMakroTitle":"Makro, Tatuapé-SP — Programador Júnior",
+  "reMakroTime":"01/2021 - 09/2022",
+  "reMakroDesc":"Trabalhei como desenvolvedor Android, onde produzi bibliotecas que disponibilizavam ferramentas de segurança para aplicações. Desenvolvi ferramentas como:<br><br> ● Ferramentas de criptografia e pinning<br> ● Análise de ambiente do dispositivo<br> ● Análises estáticas e dinâmicas das vulnerabilidades de aplicações<br> ● Testes unitários e aplicações de demonstração<br> ● Ofuscação com Dexguard, e proteção em runtime com RASP <br> ● Escrevi manuais de implementação da biblioteca para os clientes" ,
+  "reIcarusTitle":"Laboratório de inovações Icarus, Carapicuíba-SP — Monitor",
+  "reIcarusTime":"06/2019 - 12/2020",
+  "reIcarusDesc":"Trabalhei como monitor, auxiliando os alunos no desenvolvimento de seus projetos semestrais, onde desenvolviam jogos para inúmeras plataformas.<br>Também atuei como orientador de projetos focados em tecnologias emergentes como Realidade Aumentada, Realidade Virtual,  entre outras." ,
+  "reFatecTitle":"Fatec Carapicuíba, Jogos Digitais — Tecnólogo",
+  "reFatecTime":"07/2018 — 12/2021",
+  "reFatecDesc":"A faculdade foi meu ponto inicial como desenvolvedor, onde desenvolvi habilidades de programação em diversas línguas como Python, Java, e principalmente C# com foco no desenvolvimento Unity. Também foi onde comecei meus primeiros projetos extracurriculares e game jams." ,
+  "reSagaTitle":"SAGA - School of art and games — Curso profissionalizante Start",
+  "reSagaTime":"06/2019 - 12/2020",
+  "reSagaDesc":"No curso, aprendi sobre processos de edição de imagem e vídeo, a partir de plataformas como Photoshop, Premiere, After Effects, assim como também aprendi sobre softwares 3D como Maya e Substance Painter",
+  "reSkillUnity":"C# para Unity(6 anos)",
+  "reSkillJava":"Java para Android (3 anos)",
+  "reSkillTech":"Tech Art (Unity e Unreal)",
+  "reSkillImage":"Edição de imagem/vídeo (Photoshop, Illustrator, Premiere, After Effects)",
+  "reSkillUnityBackend":"Backend (NodeJS, GoLang)",
+  "reSkillUnityGit":"Github (Gitflow, Pipelines)",
+  "poSimMicsPos":"Programador Lider - Backend",
+  "poSimRaqPos":"Programador Lider - Backend",
+  "poStandPos":"Programador Lider - Artista Técnico",
+  "poLooopsPos":"Programador Lider - Artista Técnico - Designer de Som",
+  "poHeirsPos":"Programador Lider",
+  "poSdanitPos":"Artista Técnico",
+  "poDoorsPos":"UI - Compositor",
+  "poDataPos":"Artista Técnico - Animador - Compositor",
+  "poEscaladaPos":"UI - Programador"
+}
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
@@ -157,3 +299,61 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+let translations = {};
+
+// async function loadTranslations(lang) {
+//   try {
+//       const response = await fetch(`assets/js/translations/${lang}.json`);
+//       if (!response.ok) {
+//           throw new Error('Translation file not found');
+//       }
+//       return await response.json();
+//   } catch (error) {
+//       console.error('Error loading translations:', error);
+//       return {};
+//   }
+// }
+
+async function applyTranslations(lang) {
+
+  // if (!translations[lang]) {
+  //   console.log("loading tranlations");
+  //   translations[lang] = await loadTranslations(lang);
+  // }
+  
+  if(lang == "en"){
+    translations[lang] = language_en
+  }
+  else{
+    translations[lang] = language_pt;
+  }
+
+  console.log("loaded tranlations");
+  const langData = translations[lang];
+  for (const [id, text] of Object.entries(langData)) {
+      const element = document.getElementById(id);
+      if (element) {
+          element.innerHTML = text;
+      }
+  }
+
+}
+
+async function init() {
+    const savedLang = localStorage.getItem('app-language') || 'en';
+    document.getElementById('language-select').value = savedLang;
+    await applyTranslations(savedLang);
+    
+    // Handle language change
+    document.getElementById('language-select').addEventListener('change', async (e) => {
+        const lang = e.target.value;
+        localStorage.setItem('app-language', lang);
+        await applyTranslations(lang);
+    });
+}
+
+
+
+init()
+
